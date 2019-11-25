0fecb9302e2d145b30382957338d44c08ef27ba0
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-25 14:05:19 -0300

    Persistindo objeto no banco

---
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoConsultaCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class InclusaoConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = context.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Brasileira");
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Japonesa");
+
+        cadastroCozinha.adicionar(cozinha1);
+        cadastroCozinha.adicionar(cozinha2);
+    }
+}
