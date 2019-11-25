2887c8f872a8cf141556bee795933ea633651be3
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-25 13:49:30 -0300

    Consultando objetos

--- src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -1,4 +1,19 @@
 package com.algaworks.algafood.jpa;
 
+import com.algaworks.algafood.AlgafoodApiApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
 public class ConsultaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = context.getBean(CadastroCozinha.class);
+
+        cadastroCozinha.listar().forEach(c -> System.out.println(c.getNome()));
+    }
 }
