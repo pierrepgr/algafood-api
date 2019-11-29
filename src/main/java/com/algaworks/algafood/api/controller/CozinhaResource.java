8cc0fb4ed3731259c11970bc601a48888ed1f3c4
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-29 10:09:58 -0300

    Consultando Singleton resource

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaResource.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaResource.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class CozinhaResource {
     @GetMapping
     public List<Cozinha> listar() {
         return this.cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscarPorId(@PathVariable("id") Long id) {
+        return this.cozinhaRepository.buscarPorId(id);
     }
 }
