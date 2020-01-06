59aa012c4cf01f62a446e6f5e1259eee3d574ce9
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2020-01-06 08:50:42 -0300

    Implementando Specifications com SDJ

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/spec/RestauranteComNomeSemelhanteSpec.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+public class RestauranteComNomeSemelhanteSpec implements Specification<Restaurante> {
+
+    private String nome;
+
+    @Override
+    public Predicate toPredicate(Root<Restaurante> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+        return criteriaBuilder.like(root.get("nome"), "%" + this.nome +"%");
+    }
+}
